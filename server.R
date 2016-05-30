dff18c7c0e215ce57080985e7d655abcbd98a3e1
Jessica Wojciechowski <jessica.wojciechowski@mymail.unisa.edu.au>
2016-05-30 18:55:36 -0400

    Warning message update
    Added a warning message for the Rumack-Matthew nomogram demonstration
    specifying if paracetamol concentration sampling is too early.

--- server.R
+++ server.R
@@ -172,6 +172,8 @@ shinyServer(function(input,output,session) {
 	output$DEMOtextOutput <- renderText({
 		if (input$DEMO_PAC >= rule.data$CONCrm[rule.data$TIME == input$DEMO_TIME] & input$DEMO_TYPE == 1) {
 			text <- "Give N-acetylcysteine according to the Rumack-Matthew Nomogram"
+		} else if (input$DEMO_TIME < 4) {
+			text <- "Sampling is too early to use the Rumack-Matthew Nomogram."
 		} else {
 			text <- "No requirement for N-acetylcysteine according to the Rumack-Matthew Nomogram"
 		}
